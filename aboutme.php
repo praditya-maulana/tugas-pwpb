906593950781bca728220de9f4f29f66dfc991d6
praditya-maulana <63705309+praditya-maulana@users.noreply.github.com>
2020-04-17 14:10:29 +0700

    Add files via upload

--- aboutme.php
+++ aboutme.php
@@ -1,6 +1,6 @@
 <?php
 echo'Nama : Praditya Maulana Yudistira <br>';
 echo'Kelas : XI RPL 1 <br>';
-echo'Asal Sekolah : SMK Taruna Bhakti <br>';
-echo'Alamat Rumah : Jl.Radar Auri Gg.Anggrek RT002/05 Cisalak,Cimanggis - Depok <br>';
+echo'No.hp : 085640821433 <br>';
+echo'Email : maulanayudistira2805@gmai.com/ pradityamaulana28@gmail.com';
 ?>
